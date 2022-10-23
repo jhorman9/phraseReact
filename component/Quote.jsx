f020658b00093d2dc83f9abbed06e0c452e31d2c
jhorman9 <jhosmita9@gmail.com>
2022-10-22 22:04:44 -0500

    actualizando

--- component/Quote.jsx
+++ component/Quote.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Quote = (quotesItem) => {
     return (
-        <div className='quoteItems'>
+        <div className='quotePhrase'>
             <i class="fa-solid fa-quote-left" id='quote'></i> {quotesItem.quotesItem}
         </div>
     );
